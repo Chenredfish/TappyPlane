f726d4b24db9e4af2fe7d1f974851a69b32a370e
redfish <chenredfish940309@gmail.com>
2024-06-17 12:37:56 +0800

    增加遊戲中止

--- main.gd
+++ main.gd
@@ -4,6 +4,8 @@ var timer:Timer = Timer.new()
 @export var min_spawn_rock_time := 1.0
 @export var max_spawn_rock_time := 3.0
 
+@onready var plane = %plane
+
 var s_rock = preload("res://src/entities/rock.tscn") #返回路徑，可以從場景那裡拖出來
 
 # Called when the node enters the scene tree for the first time.
@@ -15,15 +17,39 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if plane.position.y <=0 or plane.position.y >= get_viewport_rect().size.y:
+		game_over()
+
+
+
+func spawn_rock()->void:
+	var a_rock :Node2D= s_rock.instantiate()
+	a_rock.rock_entered.connect(_on_rock_entered)
+	#print("wait_time:", timer.wait_time)
+	var random_choice = randi_range(0,1)
+	if random_choice ==0:
+		a_rock.position = Vector2(632,randi_range(216,336))
+	else:
+		a_rock.position = Vector2(632,randi_range(-24,112))
+		a_rock.rotation_degrees = 180
+	
+	self.add_child(a_rock)
+
+func game_over():
+	get_tree().paused = true
+	plane.queue_free()
+	for rock in get_tree().get_nodes_in_group("rock"):
+		rock.queue_free()
+		
+	print("game over")
+
+
+	
 
 func _on_timer_timeout()->void:
 	spawn_rock()
 	timer.wait_time = randf_range(min_spawn_rock_time, max_spawn_rock_time)
 	timer.start()
-
-func spawn_rock()->void:
-	#print("wait_time:", timer.wait_time)
-	var a_rock :Node2D= s_rock.instantiate()
-	a_rock.position = Vector2(632,216)
-	self.add_child(a_rock)
+	
+func _on_rock_entered()->void:
+	game_over()
--- src/entities/rock.gd
+++ src/entities/rock.gd
@@ -3,6 +3,14 @@ extends CharacterBody2D
 
 const SPEED = -300.0
 
+signal rock_entered
+
 func _physics_process(delta):
 	self.velocity.x =SPEED
 	move_and_slide()
+	if self.position.x <= -56:
+		self.queue_free()
+
+func _on_area_2d_body_entered(body):
+	#if body.is_in_group(""): 檢測在組別裡面
+	rock_entered.emit()
