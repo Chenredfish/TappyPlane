80f41b6af96c272b4d9bc61569fb3d1fd7da384e
redfish <chenredfish940309@gmail.com>
2024-06-20 09:54:01 +0800

    狀態機+開始遊戲退出遊戲設定音量三種狀態實現

--- main.gd
+++ main.gd
@@ -4,9 +4,11 @@ extends Node2D
 @export var min_spawn_rock_time := 1.0
 @export var max_spawn_rock_time := 3.0
 
-@onready var plane = %plane
-@onready var game_form = %GameForm
-
+@onready var plane
+@onready var audio_game_over = $Audio_game_over
+@onready var game_state_machine :GameStateMachine= %GameStateMachine
+@onready var ui_layer = %UILayer
+#@onready var game_form = %GameForm
 
 var s_rock = preload("res://src/entities/rock.tscn") #返回路徑，可以從場景那裡拖出來
 var s_plane = preload("res://src/entities/plane.tscn")
@@ -15,8 +17,15 @@ var current_score :int = 0
 var score_timer:Timer = Timer.new()
 var timer:Timer = Timer.new()
 
+func init_game():
+	pass
+	
+func ready_game():
+	ui_layer.ready_game()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	game_state_machine.launch()
 	timer.timeout.connect(_on_timer_timeout)
 	timer.one_shot = true
 	timer.wait_time = randf_range(min_spawn_rock_time, max_spawn_rock_time)
@@ -24,27 +33,40 @@ func _ready():
 	timer.start()
 	score_timer.timeout.connect(_on_score_timer_timeout)
 	self.add_child(score_timer)
-	new_game()
+	#new_game()
 
 func new_game()->void:
+	game_state_machine.set_value('is_new_game', true)
+	
+
+func  retry_game()->void:
+	
 	if not plane:
 		plane = s_plane.instantiate()
 		plane.position = Vector2(70, 177)
 		plane.scale = Vector2(0.5,0.5)
 		self.add_child(plane)
 	
-	
-	
 	current_score = 0
-	game_form.update_socre_display(current_score)
-	
+	#game_form.update_socre_display(current_score)
+	ui_layer.update_score_display(current_score)
 	score_timer.wait_time = 1
 	score_timer.start()
-	game_form.retry_game()
+	#game_form.retry_game()
+	ui_layer.retry_game(current_score)
+	
 	get_tree().paused = false
+	
+func quit_game():
+	game_state_machine.set_value('is_quit_game', true)
+	
+func end_game():
+	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if not plane:
+		return
 	if plane.position.y <=0 or plane.position.y >= get_viewport_rect().size.y:
 		game_over()
 
@@ -60,7 +82,6 @@ func spawn_rock()->void:
 	else:
 		a_rock.position = Vector2(632,randi_range(-24,112))
 		a_rock.rotation_degrees = 180
-	
 	self.add_child(a_rock)
 
 func game_over():
@@ -69,7 +90,9 @@ func game_over():
 	for rock in get_tree().get_nodes_in_group("rock"):
 		rock.queue_free()
 		
-	game_form.game_over()
+	audio_game_over.play()
+	#game_form.game_over()
+	ui_layer.game_over()
 	#print("game over")
 
 
@@ -85,8 +108,8 @@ func _on_rock_entered()->void:
 
 func _on_score_timer_timeout()->void:
 	current_score +=1
-	game_form.update_socre_display(current_score)
-
+	#game_form.update_socre_display(current_score)
+	ui_layer.update_score_display(current_score)
 
 func _on_game_form_quit_pressed():
 	get_tree().quit()
@@ -94,3 +117,9 @@ func _on_game_form_quit_pressed():
 
 func _on_game_form_retry_pressed():
 	new_game()
+
+func _on_ui_layer_btn_new_game_pressed():
+	new_game()
+
+func _on_ui_layer_btn_quit_game_pressed():
+	quit_game()
--- src/entities/plane.gd
+++ src/entities/plane.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var audio_stream_player = $AudioStreamPlayer
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity :int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var min_tilt_angle :int= -45
@@ -17,5 +19,11 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _unhandled_input(event):
+	#play_audio()
 	if event.is_action_pressed("flap"):
 		self.velocity.y = clamp(self.velocity.y-flip_power, max_velocity*-1, max_velocity) #向上的力
+
+
+#背景音樂
+func play_audio():
+	audio_stream_player.play()
